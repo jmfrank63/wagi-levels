f03bc87418d31df6d7d1b94ca313809aa2b1b2f9
Johannes Maria Frank <jmfrank63@gmail.com>
2023-01-16 21:26:59 +0000

    Simple serving level1

--- level1/src/main.rs
+++ level1/src/main.rs
@@ -1,10 +1,12 @@
-use http::{Response, Error};
+use http::{Response, Error, StatusCode};
 use http::header::{HeaderValue, CONTENT_TYPE, CONTENT_LENGTH};
 use std::fmt;
 
-struct DisplayResponse(Response<String>);
+struct WagiResponse<B>(http::Response<B>);
 
-impl fmt::Display for DisplayResponse {
+impl<B> fmt::Display for WagiResponse<B>
+    where B: fmt::Display
+{
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let headers = self.0.headers();
         let status = self.0.status();
@@ -18,13 +20,26 @@ impl fmt::Display for DisplayResponse {
 }
 
 fn main() -> Result<(), Error> {
-    let response = build_response(200, "Pong!".into())?;
-    println!("{response}");
+    // let response = build_response(200, "Pong!".into())?;
+    let body = "Pong!";
+    let status = StatusCode::OK;
+    let response = Response::builder()
+        .version(http::Version::HTTP_11)
+        .status(status)
+        .header(CONTENT_TYPE, HeaderValue::from_static("text/html"))
+        .header(CONTENT_LENGTH, HeaderValue::from_str(&body.as_bytes().len().to_string())?)
+        .body(body)?;
+    let (parts, body) = response.into_parts();
+    println!("{:?} {}", parts.version, parts.status);
+    for (key, value) in parts.headers {
+        println!("{}: {}\r\n", key.unwrap().as_str(), value.to_str().unwrap());
+    }
+    println!("\r\n{}", body);
     Ok(())
 }
 
-fn build_response(status: u16, body: String) -> Result<DisplayResponse, Error> {
-    Ok(DisplayResponse(Response::builder()
+fn _build_response(status: u16, body: String) -> Result<WagiResponse<String>, Error> {
+    Ok(WagiResponse(Response::builder()
         .status(status)
         .header(CONTENT_TYPE, HeaderValue::from_static("text/html"))
         .header(CONTENT_LENGTH, HeaderValue::from_str(&body.as_bytes().len().to_string())?)
